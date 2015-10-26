15d2d065b33eeead7c9bdecc9eed42b27eb3772c
JockFax32 <foxtjack@gmail.com>
2015-10-25 23:19:19 -0700

    Commit

--- js/kittens.js
+++ js/kittens.js
@@ -1,25 +1,27 @@
 // JavaScript for CRUD Challenge
 // Check out Bootbox http://bootboxjs.com/
+// Thumbs up and down are glyphicons
+
 
 // Initialize Parse App 
-Parse.initialize('S7SwwjBTgEyZnJjjZ6V9YB5HItXoT3alIxzEQCKi','izqCDsoySYLvJ9nsPlCpwvSxRzMpEgv39FQpMHz3')
+Parse.initialize('S7SwwjBTgEyZnJjjZ6V9YB5HItXoT3alIxzEQCKi','izqCDsoySYLvJ9nsPlCpwvSxRzMpEgv39FQpMHz3');
 
 var Kittens = Parse.Object.extend('Kittens');
 
 $('form').submit(function(){
-    
+
     var kittens = new Kittens();
 
-    kittens.set('review-title' , $("#review-title").val());
+    $(this).find('.form-control').each(function(){
+        kittens.set($(this).attr('id'),$(this).val())
+        $(this).val('');
+    });
 
-    // $(this).find('input').each(function(){
-    //     kittens.set($(this).attr('id'),$(this).val())
-    //     $(this).val('');
-    // });
+    // kittes.set('stars',$("#star").raty());
 
     kittens.save(null,{
         success:function(){
-            getData()
+            getData();
         }
     });
 
@@ -29,7 +31,8 @@ $('form').submit(function(){
 var getData = function (){
     var query = new Parse.Query(Kittens);
 
-    query.notEqualTo('review-text','');
+    query.exists('reviewText');
+
 
 
     query.find({
@@ -40,22 +43,30 @@ var getData = function (){
 }
 
 var buildList = function (data){
+    console.log('buildList: ' , data);
     $('ol').empty();
-
+    
     data.forEach(function(d){
-        addItem;
+        addItem(d);
     })
 
 }
 
 var addItem = function (item){
-    var title = item.get('review-title');
-    var review = item.get ('review-text');
+    var title = item.get('reviewTitle');
+    var review = item.get ('reviewText');
 
-    var li = $('<li>Title: ' + title + 'Text: ' + review + '</li>');
-
+    var well =$('<div class="well well-sm"><h3>'+ title +'</h3>'+review+'</div>')
+    $('ol').append(well);
     
 
+
+
+
+
+    // var li = $('<li>Title: ' + title + 'Text: ' + review + '</li>');
+     
+    
 }
 
 getData();
