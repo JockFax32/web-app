837ce610213f0a3ecb6d9dd56e7b93381c02627a
JockFax32 <foxtjack@gmail.com>
2015-10-27 23:14:09 -0700

    Commit

--- js/kittens.js
+++ js/kittens.js
@@ -8,47 +8,35 @@ Parse.initialize('S7SwwjBTgEyZnJjjZ6V9YB5HItXoT3alIxzEQCKi','izqCDsoySYLvJ9nsPlC
 
 var Kittens = Parse.Object.extend('Kittens');
 
-$('#star').raty({
-   
+$('#starInput').raty({   
 });
+
 
 $('form').submit(function(){
 
     var kittens = new Kittens();
+    var numStars = $("#starInput").raty('score');
     
-    var numStars = $("#star").raty('score');
-
-
     kittens.set('stars',numStars);
 
     $(this).find('.form-control').each(function(){
         kittens.set($(this).attr('id'),$(this).val())
-        $(this).val('');
+        $(this).val(''); 
 
     });
 
     kittens.save(null,{
         success:function(){
             getData();
-            // console.log(($("#star").raty('score')));
-            console.log(numStars);
-
-
-
-                // + ' ' + parseInt($("#star"),10));
         }
     });
-
+    numStars.raty('stars');
     return false;
 })
 
 var getData = function (){
     var query = new Parse.Query(Kittens);
-
-    query.exists('reviewText');
-
-
-
+    query.exists('reviewText','stars');
     query.find({
         success:function(response){
             buildList(response);
@@ -57,29 +45,33 @@ var getData = function (){
 }
 
 var buildList = function (data){
-    // console.log('buildList: ' , data);
     $('ol').empty();
-    
     data.forEach(function(d){
         addItem(d);
     })
-
 }
 
 var addItem = function (item){
+    var rating = item.get('stars');
     var title = item.get('reviewTitle');
     var review = item.get ('reviewText');
-
-    var well =$('<div class="well well-sm"><h3>'+ title +'</h3>'+review+'</div>')
-    $('ol').append(well);
     
+    // $('#starReview').raty({
+
+    // var well =$('<div class="well well-sm"><div id="starReview"><h3>'
+    //             + title +'</h3>'+
+    //             '<p>'+review +'</p>'+ 
+    //             '</div>');
+    var pastReveiw =$('<div id="starReview">'+($('#starReview').raty())+'</div>');
 
 
 
 
+    
+//'<div id="starReview">'+ ($("#star").raty('score',rating))+ '</div>'+
 
-    // var li = $('<li>Title: ' + title + 'Text: ' + review + '</li>');
-     
+    $('ol').append(pastReveiw);
+    //wrap the well div around the raty?
     
 }
 
